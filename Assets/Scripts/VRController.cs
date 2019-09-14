af5fee294ce119bfc2c2a8f73f04d663993bca08
Niko Lauron <nlauron@telus.net>
2019-09-14 16:18:33 -0700

    Basic movement

--- Assets/Scripts/VRController.cs
+++ Assets/Scripts/VRController.cs
@@ -8,7 +8,7 @@ public class VRController : MonoBehaviour
     public float m_Sensitivity = 0.1f;
     public float m_MaxSpeed = 1.0f;
 
-    public SteamVR_Action_Boolean m_MoveTilt = null;
+    //public SteamVR_Action_Boolean m_MoveTilt = null;
     public SteamVR_Action_Vector2 m_MoveValue = null;
 
     private float m_Speed = 0.0f;
@@ -30,6 +30,7 @@ public class VRController : MonoBehaviour
 
     private void Update()
     {
+        print(m_MoveValue.axis);
         HandleHead();
         CalculateMovement();
         HandleHeight();
@@ -48,11 +49,35 @@ public class VRController : MonoBehaviour
 
     private void CalculateMovement()
     {
+        Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
+        Quaternion orientation = Quaternion.Euler(orientationEuler);
+        Vector3 movement = Vector3.zero;
 
+        if ( m_MoveValue.axis.y >= 0.75f || m_MoveValue.axis.y <= -0.75f)
+        {
+            m_Speed += m_MoveValue.axis.y * m_Sensitivity;
+            m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
+
+            movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+        }
+
+        m_CharacterController.Move(movement);
     }
 
     private void HandleHeight()
     {
+        float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+        m_CharacterController.height = headHeight;
 
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = m_CharacterController.height / 2;
+        newCenter.y += m_CharacterController.skinWidth;
+
+        newCenter.x = m_Head.localPosition.x;
+        newCenter.z = m_Head.localPosition.z;
+
+        newCenter = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * newCenter;
+
+        m_CharacterController.center = newCenter;
     }
 }
