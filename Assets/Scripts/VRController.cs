4bdabc1419b8a95ca2f36331532ade62f9ee729a
Niko Lauron <nlauron@telus.net>
2019-09-14 14:23:57 -0700

    Movement WIP

---
+++ Assets/Scripts/VRController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRController : MonoBehaviour
+{
+    public float m_Sensitivity = 0.1f;
+    public float m_MaxSpeed = 1.0f;
+
+    public SteamVR_Action_Boolean m_MoveTilt = null;
+    public SteamVR_Action_Vector2 m_MoveValue = null;
+
+    private float m_Speed = 0.0f;
+
+    private CharacterController m_CharacterController = null;
+    private Transform m_CameraRig = null;
+    private Transform m_Head = null;
+
+    private void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        m_CameraRig = SteamVR_Render.Top().origin;
+        m_Head = SteamVR_Render.Top().head;
+    }
+
+    private void Update()
+    {
+        HandleHead();
+        CalculateMovement();
+        HandleHeight();
+    }
+
+    private void HandleHead()
+    {
+        Vector3 oldPosition = m_CameraRig.position;
+        Quaternion oldRotation = m_CameraRig.rotation;
+
+        transform.eulerAngles = new Vector3(0.0f, m_Head.rotation.eulerAngles.y, 0.0f);
+
+        m_CameraRig.position = oldPosition;
+        m_CameraRig.rotation = oldRotation;
+    }
+
+    private void CalculateMovement()
+    {
+
+    }
+
+    private void HandleHeight()
+    {
+
+    }
+}
--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 JoystickPosition
+        {
+            get
+            {
+                return SteamVR_Actions.default_JoystickPosition;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
